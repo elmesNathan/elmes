f9936c5ff7cfb5b631e6f0ad779053250dd96b33
lisongo <105144299+elmesNathan@users.noreply.github.com>
2022-05-25 07:10:40 +0200

    Delete blog.php

--- blog.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$page = "Blog";
-$label = "Des articles pour répondre à vos préoccupations...";
-$titre = "Articles d'ELMES";
-$data_classications = array('1' => 'popularite', '2' => 'categories', '3' => 'auteurs', '4' => 'problematiques', '5' =>'titres');
-
-include_once 'models/connexion_sql.php';
-include_once 'controleurs/blog/blog.php';
