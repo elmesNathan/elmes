fd6d92bae18974d6559b27ee59f2d5be4d505171
lisongo <105144299+elmesNathan@users.noreply.github.com>
2022-05-25 07:10:59 +0200

    Delete contact.php

--- contact.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$page = "Contact";
-$label = "Nous contacter ici...";
-$titre = "ELMES 100% disponible";
-
-include_once 'models/connexion_sql.php';
-include_once 'controleurs/contact/contact.php';
